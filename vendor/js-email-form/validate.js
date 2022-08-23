67e95a01b3aaa4e75ba9f30be7a89091a17e2862
Techstop Solution <110761592+techstopsolution@users.noreply.github.com>
2022-08-23 19:13:59 +0530

    smtps.js send mail in js

--- vendor/js-email-form/validate.js
+++ vendor/js-email-form/validate.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 
     // validatetion the comment form when it is submitted
     function isEmail(email) {
-		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		return regex.test(email);
-	}
-	
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+
 	$("#submit").click(function() {
+
         $(".error").text("");
+
 		if ($("#name").val() == "") {
 			$(".name-error").text("Please enter your name");
 		}
@@ -19,7 +21,7 @@ $(document).ready(function() {
                 $(".email-error").text("Please enter a valid mail id");
             }
         }
-		if ($("#number").val() == "") {
+        if ($("#number").val() == "") {
 			$(".number-error").text("Please enter your phone number");
 		}
         else if($("#number").length > 0){
@@ -27,97 +29,47 @@ $(document).ready(function() {
                 $(".number-error").text("Please enter a valid phone number");
             }
         }
-		if ($("#subject").val() == "") {
-			$(".subject-error").text("Please enter your email address");
+        if ($("#subject").val() == "") {
+			$(".subject-error").text("Please enter your subject");
 		}
         if ($("#message").val() == "") {
-			$(".message-error").text("Please enter your email address");
+			$(".message-error").text("Please enter your message");
 		}
+
         else {
             // $(".contactusForm").submit();
+            // SMTP JS
+            var name = $('#name').val();
+            var email = $('#email').val();
+            var number = $('#number').val();
+            var subject = $('#subject').val();
+            var message = $('#message').val();
+            var Body='Name: '+name+'<br>Email: '+email+'<br>Phone No.: '+number+'<br>Subject: '+subject+'<br>Message: '+message;
+
+            // alert(Body);
+
+            Email.send({
+                SecureToken : "9bc6a945-2cb6-4da3-8050-56377dfade60",
+
+                To : 'parmarpratapsinh123@gmail.com',
+                From : "hostingwork.space@gmail.com",
+                Subject : "New message on contact from "+name,
+                Body : Body
+            }).then(
+                message =>{
+                    if(message=='OK'){
+                        $(".php-email-form").trigger("reset");
+                        // $(".sent-message").css("display", "block");
+                        alert("done");
+                    }
+                    else{
+                        console.error (message);
+                        $(".php-email-form").trigger("reset");
+                        // $(".error-message").css("display", "block");
+                        alert("cansal");
+                    }
+                }
+            );
         }
 	});
 });
-
-
-
-
-// function isEmail(email) {
-//     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-//     return regex.test(email);
-// }
-
-// $("#submit").click(function() {
-//     var errorMessage = "";
-//     var  fieldsMissing = "";
-
-//     if ($("#name").val() == "") {
-//         $("#name").css("background-color","#ffb3b3");
-//         fieldsMissing += "Name, ";
-//     }
-//     if ($("#email").val() == "") {
-//         $("#email").css("background-color","#ffb3b3");
-//         fieldsMissing += "Email, ";
-//     }
-//     if ($("#number").val() == "") {
-//         $("#number").css("background-color","#ffb3b3");
-//         fieldsMissing += "Number, ";
-//     }
-//     if ($("#subject").val() == "") {
-//         $("#subject").css("background-color","#ffb3b3");
-//         fieldsMissing += "Subject, ";
-//     }
-//     if ($("#message").val() == "") {
-//         $("#message").css("background-color","#ffb3b3");
-//         fieldsMissing += "Message, ";
-//     }
-//     if (fieldsMissing != "") {
-//         errorMessage +=  fieldsMissing + " field is Empty<br>";
-//     }
-//     if (isEmail($("#email").val()) == false) {
-//         errorMessage += "email address, ";
-//         $("#email").css("background-color","#ffb3b3");
-//     }
-//     if ($.isNumeric($("#number").val()) == false) {
-//         errorMessage += "phone number, ";
-//         $("#number").css("background-color","#ffb3b3");
-//     }
-//     if (errorMessage != "") {
-//         $("#errorMessage").html("Your " + errorMessage + " is not valid");
-//         $("#errorMessage").css("display","block");
-//         $("#sentMessage").css("display","none");
-//     }
-//     else {
-//         $("#errorMessage").css("display","none");
-        
-//             // SMTP JS
-//             var name = $('#name').val();
-//             var email = $('#email').val();
-//             var number = $('#number').val();
-//             var subject = $('#subject').val();
-//             var message = $('#message').val();
-//             var Body='Name: '+name+'<br>Email: '+email+'<br>Phone No.: '+number+'<br>Subject: '+subject+'<br>Message: '+message;
-            
-//             Email.send({
-//                 SecureToken : "a2a97aea-b709-45a8-9f36-1f39ff351461",
-//                 To : 'pratapsinhparmar.contact@gmail.com',
-//                 From : "hostingwork.space@gmail.com",
-//                 Subject : "New message on contact from "+name,
-//                 Body : Body
-//             }).then(
-//                 message =>{
-//                     if(message=='OK'){
-//                         $(".php-email-form").trigger("reset");
-//                         $(".sent-message").css("display", "block");
-//                     }
-//                     else{
-//                         console.error (message);
-//                         $(".php-email-form").trigger("reset");
-//                         $(".error-message").css("display", "block");
-//                     }
-//                 }
-//             );
-        
-
-//     }
-// });
