2a7c8137b4331f1bcb1d6877b1294674b0bc0b6e
Pratapsinh Parmar <techstopsolution@gmail.com>
2022-10-13 21:31:31 +0530

    ok counter salu thayu

--- quantquality/js/script.js
+++ quantquality/js/script.js
@@ -72,18 +72,18 @@ $(document).ready(function(){
 
 });
 
-// function visible(partial) {
-//     var $t = partial,
-//     $w = jQuery(window),
-//     viewTop = $w.scrollTop(),
-//     viewBottom = viewTop + $w.height(),
-//     _top = $t.offset().top,
-//     _bottom = _top + $t.height(),
-//     compareTop = partial === true ? _bottom : _top,
-//     compareBottom = partial === true ? _top : _bottom;
+function visible(partial) {
+    var $t = partial,
+    $w = jQuery(window),
+    viewTop = $w.scrollTop(),
+    viewBottom = viewTop + $w.height(),
+    _top = $t.offset().top,
+    _bottom = _top + $t.height(),
+    compareTop = partial === true ? _bottom : _top,
+    compareBottom = partial === true ? _top : _bottom;
 
-//     return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
-// }
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+}
 
 // About Counter counting
 $(window).scroll(function(){
