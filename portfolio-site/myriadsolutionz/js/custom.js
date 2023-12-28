aea7b74c2d8fd6eb87d49fe2e9a74c4339470511
pratapsinhparmar <techstopsolution@gmail.com>
2023-12-28 21:31:45 +0530

    update

--- portfolio-site/myriadsolutionz/js/custom.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("my-navbar-all");
-    if (x.className === "navbar-all") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar-all";
-    }
-}
